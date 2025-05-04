7b634406a79a86109e5d220c8696abd36a808296
Ilkhom I. Melibaev <107622192+fluttberry@users.noreply.github.com>
2025-05-04 17:37:13 +0600

    profile screen ui with log out function is done

--- lib/ui/pages/main/profile_screen.dart
+++ lib/ui/pages/main/profile_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:insta_app/repository/profile_repository.dart';
+import 'package:insta_app/ui/const/route.dart';
+import 'package:insta_app/ui/pages/auth/login_page.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -17,6 +20,96 @@ class _ProfileScreenState extends State<ProfileScreen> {
   ProfileRepository _profileRepository = ProfileRepository();
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile Screen'));
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Center(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                height: 120,
+                width: 120,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.grey.shade300,
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                color: Colors.grey.shade300,
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Name...',
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                color: Colors.grey.shade300,
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Nickname...',
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                color: Colors.grey.shade300,
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'City...',
+                      hintStyle: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 50,
+
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.grey.shade300,
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: Center(child: Text('Save')),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              Container(
+                height: 50,
+
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Colors.grey.shade300,
+                ),
+                child: InkWell(
+                  onTap: () async {
+                    MRoute.replce(context, LoginPage());
+                    await FirebaseAuth.instance.signOut();
+                  },
+                  child: Center(child: Text('Log out')),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-} 
+}
